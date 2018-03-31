8eae003a27c671dc8c4838c9e36e7187858ed443
kid0kh <kreokh@gmail.com>
2018-03-31 21:56:13 +0300

    create project

---
+++ ntp_verify.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Check if the script is being run by root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# Check if ntp service is up and running
+
+result=$(ps ax | grep ntpd | grep -v grep > /dev/null)
+
+if [ $? -eq 1 ]
+
+	then
+        service ntp start
+	else
+        echo "All is ok" > /dev/null
+fi
+
+# Check if changes were made
+
+result=$(diff -u0B /etc/ntp.conf /etc/ntp.conf_default)
+
+if [ $? -eq 1 ]
+
+	then
+	echo "NOTICE: /etc/ntp.conf was changed"
+	echo $result
+        cat /etc/ntp.conf_default > /etc/ntp.conf && service ntp restart
+	else
+        echo "All is ok" > /dev/null
+	fi
